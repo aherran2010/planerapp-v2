49396ec774a78ba4631c40853013212739dc6007
aherran <aherran@gmail.com>
2021-11-21 17:03:18 +0100

    API and Test

---
+++ src/PlannerApp.Shared/Responses/ApiErrorResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Responses
+{
+    
+        public class ApiErrorResponse
+        {
+            public string Message { get; set; }
+            public string[] Errors { get; set; }
+            public bool IsSuccess { get; set; }
+        }
+        
+
+}
---
+++ src/PlannerApp.Shared/Responses/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlannerApp.Shared.Responses
+{
+    public class ApiResponse
+    {
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+
+    //mismo objeto en un genérico y tiene carga util o 1 valor dentro de el
+    public class ApiResponse<T> : ApiResponse   //heredará de ApiResponse
+    {
+        public T Value { get; set; }    //adición tendrá valor get
+    }
+ 
+}
